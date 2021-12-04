d86964a174d6cd0b5a1230495f75df3c83d536df
unknown <italo.fs@gmail.com>
2021-12-04 14:29:53 -0300

    Aprendendo JavaScript

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+/*
+var nome = "Italo Ferreira";
+var idade = 26;
+var idade2 = 10;
+alert("Bem Vindo " + nome);
+*/
+
+
+/*
+var idade = prompt("Qual sua idade ? ")
+
+if (idade >= 18){
+    alert("Maior de Idade");
+}else{
+    alert("Menor de Idade");
+};
+*/
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por Clicar !</b>";
+    //alert("Obrigado por clicar!")
+}
+
+function redirecionar(){
+    window.open("https://bombcrypto.io/")
+}
+
+function trocar(){
+    alert("Trocar texto");
+}
+
+function funcaochange(elemento){
+    console.log(Element.value);
+}
